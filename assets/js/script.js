d8fd8d65dd0b6996811095985e61154063cfc10c
Moises Godoy <moigodoy@gmail.com.com>
2021-07-10 18:53:18 -0400

    ultimas depuraciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,4 @@
-/*$('html, body').animate({
-  scrollTop: $('section').offset().top
-}, 500);*/
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
-  /* Invisible texto */
-figcaption {
-  display:none; 
-  transition: all .5s;
-}
-/* Visible texto */
-figure:hover > figcaption {
-  display:block;
-  transition: all .5s;
-}
